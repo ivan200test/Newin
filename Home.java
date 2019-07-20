a8380a5dc94419832cf7ec0b849af640d0ac4c83
Ivan Test2000 <ivan200test@yandex.ru>
2019-07-21 03:16:15 +0400

    Home java WORLD

---
+++ Home.java
@@ -0,0 +1,11 @@
+public class Home {
+	public static void main (String[] args) {
+		int a, b, c;
+		String s1, s2;
+		System.out.println("Введите ДВА числа");
+		a = new Scanner(System.in).nextInt();
+		b = new Scanner(System.in).nextInt();
+		c = a + b;
+		System.out.println("Сумма ваших чисел равна " + c);
+	}
+}
